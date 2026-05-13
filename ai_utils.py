c2857e44b825125406d41f1485e3978758d1a1c3
Farukh Mumtaz <farukhofficial555@gmail.com>
2026-05-13 14:43:29 +0500

    Initial AI Resume Analyzer project

--- ai_utils.py
+++ ai_utils.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
-
+from fallback_data import get_fallback_response
+from ats_utils import calculate_ats_score, get_ats_feedback
 load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
@@ -31,15 +32,23 @@ Resume:
 {resume_text}
 """
 
-    response = client.chat.completions.create(
-        model="gpt-4.1-mini",
-        messages=[
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ],
-        temperature=0.3
-    )
+    try:
 
-    return response.choices[0].message.content
+        response = client.chat.completions.create(
+            model="gpt-4.1-mini",
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+            temperature=0.3
+        )
+
+        return response.choices[0].message.content
+
+    except Exception as e:
+
+        print("Error:", e)
+
+        return get_fallback_response(job_role)
